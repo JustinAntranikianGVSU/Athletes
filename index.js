95c98e949c9f2ae8dc8fdce80750d575c08ffeaf
Justin Antranikian <jantranikian@KPAHQ.NET>
2020-04-22 07:14:16 -0600

    debugging env

--- index.js
+++ index.js
@@ -12,6 +12,9 @@ app.use(express.static(path.join(__dirname, 'public')))
 // athletes api routes
 app.use('/api/athletes', require('./src/api/athletesApi'))
 
+console.log('process.env.DBConnection')
+console.log(process.env.DBConnection)
+
 app.listen(process.env.PORT || 5000)
 
 module.exports = app
